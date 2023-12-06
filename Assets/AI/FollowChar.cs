fca07a8ae9110ea55f359bfb0b0b64594f5f2dce
MatthewLWheatley <76147911+MatthewLWheatley@users.noreply.github.com>
2023-12-06 13:59:57 +0000

    push

--- Assets/AI/FollowChar.cs
+++ Assets/AI/FollowChar.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FollowChar : MonoBehaviour
 {
@@ -12,16 +13,20 @@ public class FollowChar : MonoBehaviour
     
     public Vector3 targetPos = Vector3.zero;
 
+    public float lerpSpeed = 1.0f;
 
-    private void Start()
+    private void Awake()
     {
-        player1 = GameManager.GetComponent<GameManager>().GetComponent<PlayerManager>().Player1;
-        player2 = GameManager.GetComponent<GameManager>().GetComponent<PlayerManager>().Player1;
+        GameObject[] players;
+        players =GameObject.FindGameObjectsWithTag("Player");
+        player1 = players[0];
+        player2 = players[1];
+
         if (((player1.transform.position - this.transform.position) / 2).magnitude < ((player2.transform.position - this.transform.position) / 2).magnitude)
         {
             TargetPlayer = player1;
         }
-        else 
+        else
         {
             TargetPlayer = player2;
         }
@@ -29,6 +34,29 @@ public class FollowChar : MonoBehaviour
 
     private void Update()
     {
-        this.transform.position = Vector3.Lerp(this.transform.position, TargetPlayer.transform.position, 0.5f);
+        if (((player1.transform.position - this.transform.position) / 2).magnitude < ((player2.transform.position - this.transform.position) / 2).magnitude)
+        {
+            TargetPlayer = player1;
+        }
+        else
+        {
+            TargetPlayer = player2;
+        }
+
+        float step = lerpSpeed * Time.deltaTime;
+
+        if (((TargetPlayer.transform.position + new Vector3(-1, 0, 0)) / 2).magnitude < (((TargetPlayer.transform.position + new Vector3(1, 0, 0))) / 2).magnitude)
+        {
+            targetPos = (TargetPlayer.transform.position + new Vector3(-1, 0, 0));
+        }
+        else
+        {
+
+            targetPos = (TargetPlayer.transform.position + new Vector3(1, 0, 0));
+
+        }
+        // Use Vector3.MoveTowards to move towards the target position at a constant speed.
+        this.transform.position = Vector3.MoveTowards(this.transform.position, targetPos, step);
     }
 }
+
--- Assets/PlayerManager.cs
+++ Assets/PlayerManager.cs
@@ -7,4 +7,9 @@ public class PlayerManager : MonoBehaviour
     public GameObject Player1;
     public GameObject Player2;
     public GameObject MainCamera;
+
+    public GameObject GetPlayer1() 
+    { 
+        return Player1;
+    }
 }
